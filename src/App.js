3cbba90de2d46536081ab7f11041ddef9c72d8f4
kiwan kim <dkwanm1@snu.ac.kr>
2020-07-27 11:03:18 +0900

    깃 여러계정 시험

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="Ap">
         <Switch>
           <Route path='/' exact component={Main} />
         </Switch>
